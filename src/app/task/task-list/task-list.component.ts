bbb9b3a5227ed6d614bb07657211d461f85cf8cc
iivansaanchez <ivansanchezsanchez2002@gmail.com>
2024-05-13 08:13:31 +0200

    Fase 2

--- src/app/task/task-list/task-list.component.ts
+++ src/app/task/task-list/task-list.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
-
+  tasks = [
+    {
+      id: 1,
+      title: 'Crear Presentación',
+      description: 'Preparar la presentación para el proyecto final',
+      priority: 'alta',
+      status: 'pendiente'
+    },
+    {
+      id: 2,
+      title: 'Investigar Angular',
+      description: 'Investigar sobre las características de Angular para el proyecto',
+      priority: 'media',
+      status: 'en progreso'
+    },
+    {
+      id: 3,
+      title: 'Implementar Servicios',
+      description: 'Implementar servicios para manejar datos en la aplicación',
+      priority: 'baja',
+      status: 'completada'
+    },
+    {
+      id: 4,
+      title: 'Diseñar Base de Datos',
+      description: 'Diseñar la estructura de la base de datos para el proyecto',
+      priority: 'alta',
+      status: 'pendiente'
+    }
+  ];
 }
